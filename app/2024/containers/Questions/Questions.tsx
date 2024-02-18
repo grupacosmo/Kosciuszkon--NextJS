859aa0d14d472caaaed2657c3211856828a501a2
JBR-Sapeta <jakub.ryszard.sapeta@gmail.com>
2024-02-18 13:33:06 +0100

    refactor: K2024-7 disable form and countdown

--- app/2024/containers/Questions/Questions.tsx
+++ app/2024/containers/Questions/Questions.tsx
@@ -1,19 +1,26 @@
 import { type ReactElement } from 'react';
 import { FaWpforms, FaFacebookF } from 'react-icons/fa';
 import { Collapsible } from '@/app/utils';
+
 import { HexagonLink } from './components/HexagonLink';
 import { Countdown } from './components/Countdown';
+import { calcEndTime } from './components/utils';
 import { questionsData } from './data';
+
 import styles from './Questions.module.scss';
 
-
+const START_DATE = '2024-03-05T11:00:00';
+const END_DATE = '2024-03-06T15:00:00';
 
 export function Questions(): ReactElement {
+  const disableCountdown = calcEndTime(END_DATE) < 0;
+
   return (
     <section className={styles.section}>
       <Countdown
-        startDate='2024-03-05T11:00:00'
-        endDate='2024-03-06T15:00:00'
+        startDate={START_DATE}
+        endDate={END_DATE}
+        disabled={disableCountdown}
       />
 
       <article className={styles.linksContainer}>
@@ -24,6 +31,7 @@ export function Questions(): ReactElement {
           href='https://forms.office.com/r/9AH7VzkWa4'
           alt='Czarny sześciokąt z zielonym obramowaniem i ikoną formularza.'
           Icon={FaWpforms}
+          disabled
         />
         <HexagonLink
           id='faq__fb-evennt-label'
